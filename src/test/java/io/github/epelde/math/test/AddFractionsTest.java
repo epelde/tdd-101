80257133610e60474ba9ec2c386ca44f93e55fc3
epelde <aepelde@gmail.com>
2019-04-04 16:35:13 +0200

    shorter tests by removing variable definition

--- src/test/java/io/github/epelde/math/test/AddFractionsTest.java
+++ src/test/java/io/github/epelde/math/test/AddFractionsTest.java
@@ -10,49 +10,41 @@ public class AddFractionsTest {
 
     @Test
     public void zeroPlusZero() {
-        final Fraction sum = new Fraction(0).plus(new Fraction(0));
-        assertEquals(0, sum.intValue());
+        assertEquals(0, new Fraction(0).plus(new Fraction(0)).intValue());
     }
 
     @Test
     public void nonZeroPlusZero() {
-        final Fraction sum = new Fraction(3).plus(new Fraction(0));
-        assertEquals(3, sum.intValue());
+        assertEquals(3, new Fraction(3).plus(new Fraction(0)).intValue());
     }
 
     @Test
     public void zeroPlusNonZero() {
-        final Fraction sum = new Fraction(0).plus(new Fraction(5));
-        assertEquals(5, sum.intValue());
+        assertEquals(5, new Fraction(0).plus(new Fraction(5)).intValue());
     }
 
     @Test
     public void nonZeroPlusNonZero() {
-        final Fraction sum = new Fraction(4).plus(new Fraction(6));
-        assertEquals(10, sum.intValue());
+        assertEquals(10, new Fraction(4).plus(new Fraction(6)).intValue());
     }
 
     @Test
     public void negativeIntegerPlusZero() {
-        final Fraction sum = new Fraction(-3).plus(new Fraction(0));
-        assertEquals(-3, sum.intValue());
+        assertEquals(-3, new Fraction(-3).plus(new Fraction(0)).intValue());
     }
 
     @Test
     public void zeroPlusNegativeInteger() {
-        final Fraction sum = new Fraction(0).plus(new Fraction(-9));
-        assertEquals(-9, sum.intValue());
+        assertEquals(-9, new Fraction(0).plus(new Fraction(-9)).intValue());
     }
 
     @Test
     public void negativeInputsAndNegativeOutput() {
-        final Fraction sum = new Fraction(-3).plus(new Fraction(1));
-        assertEquals(-2, sum.intValue());
+        assertEquals(-2, new Fraction(-3).plus(new Fraction(1)).intValue());
     }
 
     @Test
     public void addingNegativeIntegers() {
-        final Fraction sum =  new Fraction(-4).plus(new Fraction(-9));
-        assertEquals(-13, sum.intValue());
+        assertEquals(-13, new Fraction(-4).plus(new Fraction(-9)).intValue());
     }
 }
