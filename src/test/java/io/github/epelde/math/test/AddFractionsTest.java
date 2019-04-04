1aef7ddc73f62c52928a227b3a2151d5d11962c0
epelde <aepelde@gmail.com>
2019-04-04 15:09:42 +0200

    we can sum non zero values with zero

--- src/test/java/io/github/epelde/math/test/AddFractionsTest.java
+++ src/test/java/io/github/epelde/math/test/AddFractionsTest.java
@@ -1,14 +1,20 @@
 package io.github.epelde.math.test;
 
 import io.github.epelde.math.Fraction;
-import org.junit.Assert;
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class AddFractionsTest {
 
     @Test
     public void zeroPlusZero() {
-        Fraction sum = new Fraction(0).plus(new Fraction(0));
-        Assert.assertEquals(0, sum.intValue());
+        final Fraction sum = new Fraction(0).plus(new Fraction(0));
+        assertEquals(0, sum.intValue());
+    }
+
+    @Test
+    public void nonZeroPlusZero() {
+        final Fraction sum = new Fraction(3).plus(new Fraction(0));
+        assertEquals(3, sum.intValue());
     }
 }
