58d1e926605641c243a2bdfd37d7c6346e2dad6f
epelde <aepelde@gmail.com>
2019-04-05 09:37:22 +0200

    add tests to test suite in order to execute them all together

---
+++ src/test/java/io/github/epelde/math/test/FractionTestSuite.java
@@ -0,0 +1,9 @@
+package io.github.epelde.math.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({AddFractionsTest.class, FractionEqualsTest.class})
+public class FractionTestSuite {
+}
