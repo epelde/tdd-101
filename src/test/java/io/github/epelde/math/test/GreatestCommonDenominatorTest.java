7431d6da2e0fcc87385e1b6c2f8b429c07d16829
epelde <aepelde@gmail.com>
2019-04-07 12:06:16 +0200

    testing wikipedia gcd implementation

--- src/test/java/io/github/epelde/math/test/GreatestCommonDenominatorTest.java
+++ src/test/java/io/github/epelde/math/test/GreatestCommonDenominatorTest.java
@@ -6,8 +6,31 @@ import static org.junit.Assert.*;
 public class GreatestCommonDenominatorTest {
 
     @Test
-    public void oneAndOne() {
+    public void equalNumbersGCD() {
         assertEquals(1, gcd(1, 1));
+        assertEquals(2, gcd(2, 2));
+        assertEquals(-1, gcd(-1, -1));
+    }
+
+    @Test
+    public void relativelyPrime() {
+        assertEquals(1, gcd(4, 7));
+        assertEquals(1, gcd(3, 4));
+        assertEquals(-1, gcd(-2, -3));
+    }
+
+    @Test
+    public void oneIsMultipleOfTheOther() {
+        assertEquals(3, gcd(3, 9));
+        assertEquals(5, gcd(5, 30));
+        assertEquals(-3, gcd(-3, -9));
+    }
+
+    @Test
+    public void commonFactor() {
+        assertEquals(2, gcd(6, 8));
+        assertEquals(7, gcd(21, 56));
+        assertEquals(-12, gcd(-12, -180));
     }
 
     private int gcd(int a, int b) {
