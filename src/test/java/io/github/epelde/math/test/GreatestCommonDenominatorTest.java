4e8e4a42c964dd4c5c50e64e3bf5cc6ebfa4126f
epelde <aepelde@gmail.com>
2019-04-07 17:23:37 +0200

    extract gcd method to custom class

--- src/test/java/io/github/epelde/math/test/GreatestCommonDenominatorTest.java
+++ src/test/java/io/github/epelde/math/test/GreatestCommonDenominatorTest.java
@@ -1,5 +1,6 @@
 package io.github.epelde.math.test;
 
+import io.github.epelde.math.NumberTheory;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -7,39 +8,30 @@ public class GreatestCommonDenominatorTest {
 
     @Test
     public void equalNumbersGCD() {
-        assertEquals(1, gcd(1, 1));
-        assertEquals(2, gcd(2, 2));
-        assertEquals(1, gcd(-1, -1));
+        assertEquals(1, NumberTheory.gcd(1, 1));
+        assertEquals(2, NumberTheory.gcd(2, 2));
+        assertEquals(1, NumberTheory.gcd(-1, -1));
     }
 
     @Test
     public void relativelyPrime() {
-        assertEquals(1, gcd(4, 7));
-        assertEquals(1, gcd(3, 4));
-        assertEquals(1, gcd(-2, -3));
+        assertEquals(1, NumberTheory.gcd(4, 7));
+        assertEquals(1, NumberTheory.gcd(3, 4));
+        assertEquals(1, NumberTheory.gcd(-2, -3));
     }
 
     @Test
     public void oneIsMultipleOfTheOther() {
-        assertEquals(3, gcd(3, 9));
-        assertEquals(5, gcd(5, 30));
-        assertEquals(3, gcd(-3, -9));
+        assertEquals(3, NumberTheory.gcd(3, 9));
+        assertEquals(5, NumberTheory.gcd(5, 30));
+        assertEquals(3, NumberTheory.gcd(-3, -9));
     }
 
     @Test
     public void commonFactor() {
-        assertEquals(2, gcd(6, 8));
-        assertEquals(7, gcd(21, 56));
-        assertEquals(12, gcd(-24, -108));
-    }
-
-    private int gcd(int a, int b) {
-        while (b != 0) {
-            int t = b;
-            b = a % t;
-            a = t;
-        }
-        return Math.abs(a);
+        assertEquals(2, NumberTheory.gcd(6, 8));
+        assertEquals(7, NumberTheory.gcd(21, 56));
+        assertEquals(12, NumberTheory.gcd(-24, -108));
     }
 
 }
