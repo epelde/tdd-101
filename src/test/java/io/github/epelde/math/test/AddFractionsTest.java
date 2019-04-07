b8efe13fdd85bbb78597b074c2bf514eff69eaf3
epelde <aepelde@gmail.com>
2019-04-07 11:09:08 +0200

    group similar asserts in tests

--- src/test/java/io/github/epelde/math/test/AddFractionsTest.java
+++ src/test/java/io/github/epelde/math/test/AddFractionsTest.java
@@ -7,65 +7,46 @@ import org.junit.Test;
 public class AddFractionsTest {
 
     @Test
-    public void zeroPlusZero() {
+    public void addWholeNumbers() {
         assertEquals(new Fraction(0),
-            new Fraction(0).plus(new Fraction(0)));
-    }
-
-    @Test
-    public void nonZeroPlusZero() {
+                new Fraction(0).plus(new Fraction(0)));
         assertEquals(new Fraction(3),
-            new Fraction(3).plus(new Fraction(0)));
-    }
-
-    @Test
-    public void zeroPlusNonZero() {
+                new Fraction(3).plus(new Fraction(0)));
         assertEquals(new Fraction(5),
-            new Fraction(0).plus(new Fraction(5)));
-    }
-
-    @Test
-    public void nonZeroPlusNonZero() {
+                new Fraction(0).plus(new Fraction(5)));
         assertEquals(new Fraction(10),
-            new Fraction(4).plus(new Fraction(6)));
-    }
-
-    @Test
-    public void negativeIntegerPlusZero() {
+                new Fraction(4).plus(new Fraction(6)));
         assertEquals(new Fraction(-3),
-            new Fraction(-3).plus(new Fraction(0)));
-    }
-
-    @Test
-    public void zeroPlusNegativeInteger() {
+                new Fraction(-3).plus(new Fraction(0)));
         assertEquals(new Fraction(-9),
-            new Fraction(0).plus(new Fraction(-9)));
-    }
-
-    @Test
-    public void negativeAndPositiveInputsWithNegativeOutput() {
+                new Fraction(0).plus(new Fraction(-9)));
         assertEquals(new Fraction(-2),
-            new Fraction(-3).plus(new Fraction(1)));
-    }
-
-    @Test
-    public void addingNegativeIntegers() {
+                new Fraction(-3).plus(new Fraction(1)));
         assertEquals(new Fraction(-13),
-            new Fraction(-4).plus(new Fraction(-9)));
+                new Fraction(-4).plus(new Fraction(-9)));
     }
 
+
     @Test
-    public void equalDenominatorSum() {
+    public void addEqualDenominatorFractions() {
         assertEquals(new Fraction(3, 5),
-            new Fraction(1, 5).plus(new Fraction(2, 5)));
+                new Fraction(1, 5).plus(new Fraction(2, 5)));
+        assertEquals(new Fraction(4, 5),
+                new Fraction(6, 5).plus(new Fraction(-2, 5)));
+        assertEquals(new Fraction(-3, 6),
+                new Fraction(-1, 6).plus(new Fraction(-2, 6)));
+        assertEquals(new Fraction(3, -4),
+                new Fraction(2, -4).plus(new Fraction(1, -4)));
     }
 
     @Test
-    public void differentDenominatorSumWithoutReducing() {
+    public void addDifferentDenominatorFractionsWithoutReducing() {
         assertEquals(new Fraction(5, 6),
             new Fraction(1, 3).plus(new Fraction(1, 2)));
         assertEquals(new Fraction(21, 54),
                 new Fraction(1, 6).plus(new Fraction(2, 9)));
+        assertEquals(new Fraction(-1, 15),
+                new Fraction(3, 5).plus(new Fraction(-2, 3)));
     }
 
 }
