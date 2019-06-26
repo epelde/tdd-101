65f3cb175e0080cfc56292321b037e5f566e98b4
epelde <aepelde@gmail.com>
2019-06-26 13:27:47 +0200

    more generic echo implementation

--- src/main/java/io/github/epelde/fizzbuzz/FizzBuzzer.java
+++ src/main/java/io/github/epelde/fizzbuzz/FizzBuzzer.java
@@ -2,7 +2,6 @@ package io.github.epelde.fizzbuzz;
 
 public class FizzBuzzer {
     public String echo(int number) {
-        if (number == 1) return "1";
-        return "2";
+        return Integer.toString(number);
     }
 }
--- src/test/java/io/github/epelde/fizzbuzz/FizzBuzzerTest.java
+++ src/test/java/io/github/epelde/fizzbuzz/FizzBuzzerTest.java
@@ -10,5 +10,12 @@ public class FizzBuzzerTest {
         FizzBuzzer fizzBuzzer = new FizzBuzzer();
         Assert.assertEquals("1", fizzBuzzer.echo(1));
         Assert.assertEquals("2", fizzBuzzer.echo(2));
+        Assert.assertEquals("4", fizzBuzzer.echo(4));
+        Assert.assertEquals("7", fizzBuzzer.echo(7));
+        Assert.assertEquals("8", fizzBuzzer.echo(8));
+        Assert.assertEquals("11", fizzBuzzer.echo(11));
+        Assert.assertEquals("13", fizzBuzzer.echo(13));
+        Assert.assertEquals("19", fizzBuzzer.echo(19));
+        Assert.assertEquals("100", fizzBuzzer.echo(100));
     }
 }
